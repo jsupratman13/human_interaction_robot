ac2a2afa0224673322488675d51eb0e94f5429a6
jsupratman13 <supratmanjoshua@gmail.com>
2017-07-13 16:23:03 +0900

    added files for simulation

---
+++ hir_learning/scripts/environment.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python
+import rospy
+import math,random
+import sys,abc,time
+from gazebo_msgs.srv import *
+from control_msgs.msg import JointTrajectoryControllerState
+from trajectory_msgs.msg import JointTrajectoryPoint
+from geometry_msgs.msg import Point, Wrench, Twist
+from std_srvs.srv import Empty
+
+class Environment(object):
+    __metaclass__ = abc.ABCMeta
+
+    FORWARD = 0
+    STOP = 1
+    REVERSE = 2
+
+    def __init__(self):
+        self.sub = rospy.Subscriber('/manipulator/left_arm_controller/state', JointTrajectoryControllerState, self.__get_state)
+        self.pub = rospy.Publisher('/icart_mini/cmd_vel', Twist, queue_size=10)
+        self.apply_force = rospy.ServiceProxy('/gazebo/apply_body_wrench', ApplyBodyWrench)
+        self.sim_reset = rospy.ServiceProxy('/gazebo/reset_simulation', Empty)
+        self.clear_force = rospy.ServiceProxy('/gazebo/clear_body_wrenches',BodyRequest)
+
+        self.error = []
+        self.joint_names = []
+        self.initial_step_time = 0
+
+    def __get_state(self, msg):
+        self.joint_names =  msg.joint_names
+        self.error = msg.error.positions
+
+    def __move(self, action):
+        vel = Twist()
+        if action == Environment.FORWARD:
+            vel.linear.x = 0.5
+        elif action == Environment.STOP:
+            vel.linear.x = 0.0
+        elif action == Environment.REVERSE:
+            vel.linear.x = -0.5
+        else:
+            assert False, 'unkonwn action'
+        vel.linear.y = 0
+        vel.linear.z = 0
+        vel.angular.x = 0
+        vel.angular.y = 0
+        vel.angular.z = 0
+        self.pub.publish(vel)
+
+    def __apply_force(self):
+        try:
+            body_name = 'robot1::wrist_roll_link'
+            reference_frame = 'robot1::wrist_roll_link'
+            point = Point()
+            point.x = 0
+            point.y = 0
+            point.z = 0
+            wrench = Wrench()
+            wrench.force.x = 15
+            wrench.force.y = 0
+            wrench.force.z = 0
+            wrench.torque.x = 0
+            wrench.torque.y = 0
+            wrench.torque.z = 0
+            start_time = rospy.Time.now()
+            duration = rospy.Duration(10)
+
+            self.apply_force(body_name, reference_frame, point, wrench, start_time, duration)
+
+        except rospy.ServiceException as e:
+            rospy.loginfo('apply force failed %s', e)
+
+    def __clear_force(self):
+        try:
+            body_name = 'robot1::wrist_roll_link'
+            self.clear_force(body_name)
+        except rospy.ServiceException as e:
+            rospy.loginfo('clear force failed %s',e) 
+
+    def __reset_sim(self):
+        try:
+            self.sim_reset()
+        except rospy.ServiceException as e:
+            rospy.loginfo('reset simulation failed %s', e)
+
+    def __get_reward(self):
+        return -1 * sum(self.error)
+
+    def reset(self):
+        self.__move(Environment.STOP)
+        self.__clear_force()
+        self.__reset_sim() 
+        time.sleep(5)
+        self.__apply_force()
+        self.initial_step_time = rospy.Time.now().secs
+        return self.error
+
+    def step(self, action):
+        is_terminal = False
+        self.__move(action)
+        reward = self.__get_reward()
+        
+        if math.fabs(rospy.Time.now().secs-self.initial_step_time) > 10:
+            is_terminal = True
+
+        if reward > -0.08:
+            is_terminal = True
+
+        return self.error, reward, is_terminal
+
+    class observation_space(object):
+        def __init__(self):
+            pass
+
+        def get_size(self):
+            return 6
+
+    class action_space(object):
+        def __init__(self):
+            self.action_list = [Environment.FORWARD,
+                                Environment.STOP,
+                                Environment.REVERSE]
+        
+        def sample(self):
+            return random.choice(self.action_list)
+    
+        def get_size(self):
+            return len(self.action_list)
+
+
+if __name__ == '__main__':
+    rospy.init_node('enivornment')
+    rospy.loginfo('test')
+    try:
+        pass
+    except rospy.ROSInterruptException:
+        pass
+    rospy.spin()
---
+++ hir_learning/scripts/test.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+import numpy as np
+import gym
+import json
+import collections,random,sys
+from keras.models import model_from_json
+from keras.optimizers import Adam
+import rospy
+from environment import Environment
+
+class Agent(object):
+    def __init__(self, env, model, weight):
+        self.alpha = 0.001
+        self.env = env
+        self.ntrials = 5
+        self.nstates = env.observation_space().get_size()
+        self.model = self.load_model(model)
+        self.weight = weight
+        self.configure = {}
+
+    def load_model(self,filename):
+        json_file = open(filename,'r')
+        model = model_from_json(json_file.read())
+        json_file.close() 
+        return model
+
+    def epsilon_greedy(self,state):
+        if np.random.rand() < self.epsilon:
+            return self.env.action_space().sample()
+        else:
+            Q = self.model.predict(state)
+            return np.argmax(Q[0])
+
+    def train(self):
+        self.model.load_weights(self.weight)
+        self.model.compile(loss='mse', optimizer=Adam(lr=self.alpha))
+        self.epsilon = 0.1
+        for trial in range(self.ntrials):
+            s = self.env.reset()
+            s = np.reshape(s,[1,self.nstates])
+            treward = 0
+            while True:
+                a = self.epsilon_greedy(s)
+                s2, r, done = self.env.step(a)
+                s = np.reshape(s2, [1,self.nstates])
+                treward += r
+                if done:
+                    rospy.loginfo('test: ' + str(trial+1) + ' reward: ' + str(treward))
+                    break
+
+if __name__ == '__main__':
+    if not len(sys.argv) > 2:
+        assert False, 'missing model and/or weight'
+    rospy.init_node('ddqn_test')
+    rospy.loginfo('start testing')
+    env = Environment()
+    agent = Agent(env, str(sys.argv[1]),str(sys.argv[2]))
+    agent.train()
+    env.reset()
+    rospy.loginfo('COMPLETE TESTING')
+    rospy.spin()
