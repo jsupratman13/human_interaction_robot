0f56851a1b40cec851b43ed8adfe0ecf046a0914
jsupratman13 <supratmanjoshua@gmail.com>
2017-12-07 15:12:29 +0900

    fix online training

--- hir_learning/scripts/real_training.py
+++ hir_learning/scripts/real_training.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
-#filename: ddqn.py                             
+#filename: real_training.py                             
 #brief: double deep q-learning on neural network                  
 #author: Joshua Supratman                    
-#last modified: 2017.10.21. 
+#last modified: 2017.12.07. 
 #vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv#
-import rospy
 import numpy as np
 import json
 import matplotlib
@@ -16,12 +15,11 @@ import collections,random,sys,time
 from keras.models import Sequential, model_from_json
 from keras.layers import Dense
 from keras.optimizers import Adam
-from real_environment import Environment
 import ConfigParser
 import traceback
 
 class Agent(object):
-    def __init__(self,env):
+    def __init__(self, modelname, target_weight, target_name, sample_data):
         config = ConfigParser.RawConfigParser()
         config.read('real_parameters.cfg')
         self.gamma = config.getfloat('training','gamma')
@@ -32,74 +30,64 @@ class Agent(object):
         self.epsilon_decay = config.getfloat('epsilon_greedy', 'epsilon_decay')
         self.batch_size = config.getint('network', 'batch_size')
         self.updateQ = config.getint('network', 'update_network')
-        self.weights_name = 'episodefinal.hdf5'
-        np.random.seed(123)
-        self.env = env
-        self.nstates = env.observation_space.get_size()
-        self.nactions = env.action_space.get_size()
-        self.model = self.create_neural_network()
         self.memory = collections.deque(maxlen=config.getint('network', 'memory_size'))
-        self.target_model = self.model
+        
+        self.weights_name = target_name
+        np.random.seed(123)
+        
+        self.model = self.load_model(modelname)
+        self.model.load_weights(target_weight)
+        self.model.compile(loss='mse', optimizer=Adam(lr=self.alpha))
+        self.target_model = self.load_model(modelname)
+        self.target_model.load_weights(target_weight)
+        self.target_model.compile(loss='mse', optimizer=Adam(lr=self.alpha))
+        
         self.loss_list = []
-        self.reward_list = []
+        self.load_sample(sample_data)
 
-    def create_neural_network(self):
-        model = Sequential()
-        model.add(Dense(100,input_dim=self.nstates, activation='relu'))
-        model.add(Dense(100,activation='relu'))
-        model.add(Dense(self.nactions,activation='linear'))
-        model.compile(loss='mse', optimizer=Adam(lr=self.alpha))
-        model_json = model.to_json()
-        with open('hir_model.json','w') as json_file:
-            json_file.write(model_json)
-        return model
-
-    def load_model(self,filename, weight, epsilon):
+    def load_model(self,filename):
         json_file = open(filename,'r')
         model = model_from_json(json_file.read())
         json_file.close() 
-        self.model = model
-        self.model.load_weights(weight)
-        self.model.compile(loss='mse', optimizer=Adam(lr=self.alpha))
-        self.epsilon = epsilon
+        return model
 
-    def epsilon_greedy(self,state):
-        if np.random.rand() < self.epsilon:
-            return self.env.action_space.sample()
-        else:
-            Q = self.model.predict(state)
-            return np.argmax(Q[0])
+    def load_sample(self, filename):
+        samples = open(filename, 'r')
+        state_index = []
+        state2_index = []
+        line = samples.readline().strip('\n')
+        line = line.split(',')
+        for i in range(len(line)):
+            if line[i].endswith('_1'):
+                state_index.append(i)
+            elif line[i] == 'action':
+                action_index = i
+            elif line[i] == 'reward':
+                reward_index = i
+            elif line[i].endswith('_2'):
+                state2_index.append(i)
+            elif line[i] == 'done':
+                done_index = i
+       
+        for sample in samples:
+            line = sample.split(',')
+            s = []
+            s2 = []
+            for j in state_index:
+                s.append(float(line[j]))
+            s = np.reshape(s, [1, len(s)])
+            a = int(line[action_index])
+            r = float(line[reward_index])
+            for k in state2_index:
+                s2.append(float(line[k]))
+            s2 = np.reshape(s2, [1, len(s2)])
+            done = bool(line[done_index]=='True')
+            
+            self.memory.append((s,a,r,s2,done))
 
     def train(self):
-        max_r = -1000000
-        try:
-            while not self.env.state:
-                pass
-        except KeyboardInterrupt:
-            assert False, 'failed to get joint state'
-
-        max_r = -1000000
-        rate = rospy.Rate(20)
         for episode in range(self.nepisodes):
-            s = self.env.reset()
-            s = np.reshape(s,[1,self.nstates]) 
-            treward = []
             loss = 0
-            raw_input('start new episode')
-            while not rospy.is_shutdown():
-                if self.env.state:
-                    a = self.epsilon_greedy(s)
-                    s2, r, done = self.env.step(a)
-                    s2 = np.reshape(s2, [1,self.nstates])
-                    self.memory.append((s,a,r,s2,done))
-                    s = s2
-                    treward.append(r)
-                else:
-                    done = False
-                rate.sleep()
-                if done:
-                    break
-            treward = sum(treward)/len(treward)
 
             #save checkpoint
             self.model.save_weights('episode'+str(episode)+'.hdf5')
@@ -107,20 +95,11 @@ class Agent(object):
             #replay experience
             if len(self.memory) > self.batch_size:
                 loss = self.replay()
+            
+            print 'episode: ' + str(episode+1) + ' loss: ' + str(round(loss,4))
             self.loss_list.append(loss)
-            self.reward_list.append(treward)
-            
-            print 'episode: ' + str(episode+1) + ' reward: ' + str(treward) + ' epsilon: ' + str(round(self.epsilon,2)) + ' loss: ' + str(round(loss,4))
 
-            #Target Network
-            if not episode % self.updateQ:
-                #self.target_model = self.model
-                self.target_model.set_weights(self.model.get_weights())
-
-            #shift from explore to exploit
-            if self.epsilon > self.min_epsilon:
-                self.epsilon *= self.epsilon_decay
-            
+        # target network
         self.model.save_weights(self.weights_name)
 
     def replay(self):
@@ -133,20 +112,47 @@ class Agent(object):
             loss += self.model.train_on_batch(s,target)
         return loss/len(minibatch)
 
+    def plot(self):
+        ep = np.arange(0,self.nepisodes, 1)
+        plt.figure(1)
+        plt.plot(ep, self.loss_list)
+        plt.xlabel('episodes')
+        plt.ylabel('loss')
+        plt.yscale('log')
+        plt.savefig('loss.png')
+        #plt.show()
+
 if __name__ == '__main__':
-    rospy.init_node('online_ddqn', disable_signals=True)
-    rospy.loginfo('START ONLINE TRAINING')
-    env = Environment()
-    agent = Agent(env)
-    start_time = time.time()
-    if len(sys.argv) > 3:
-        agent.load_model(str(sys.argv[1]),str(sys.argv[2]),float(sys.argv[3]))
+    if not len(sys.argv) > 3:
+        nstate = int(sys.argv[1])
+        naction = int(sys.argv[2])
+        config = ConfigParser.RawConfigParser()
+        config.read('real_parameters.cfg')
+        alpha = config.getfloat('training', 'alpha')
+        
+        model = Sequential()
+        model.add(Dense(100,input_dim=nstate, activation='relu'))
+        model.add(Dense(100,activation='relu'))
+        model.add(Dense(naction,activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=alpha))
+        model_json = model.to_json()
+        with open('hir_model.json','w') as json_file:
+            json_file.write(model_json)
+        model.save_weights('initial_weight.hdf5')
+        
+        print 'initialize model and weight with ' + str(nstate)+' states ' + str(naction) + ' actions '
+        sys.exit()
 
     try:
+        print 'start training'
+        start_time = time.time()
+        agent = Agent(str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]), str(sys.argv[4]))
         agent.train()
         agent.plot()
-        env.reset()
-    except (KeyboardInterrupt, SystemExit):
-        pass
-    finally:
-        rospy.loginfo('EXIT TRAINING')
+        print 'COMPLETE TRAINING'
+        m,s = divmod(time.time()-start_time, 60)
+        h,m = divmod(m, 60)
+        print 'time took %d:%02d:%02d' %(h,m,s)
+    except Exception, e:
+        traceback.print_exc()
+        sys.exit()
